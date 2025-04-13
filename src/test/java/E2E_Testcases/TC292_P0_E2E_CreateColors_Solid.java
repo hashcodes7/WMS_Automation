ee25f518d80bab57537c1547733aef5ddfb4af55
Harsh Vardhan Verma <83136005+hashcodes7@users.noreply.github.com>
2025-04-14 03:23:34 +0530

    Update TC292_P0_E2E_CreateColors_Solid.java

--- src/test/java/E2E_Testcases/TC292_P0_E2E_CreateColors_Solid.java
+++ src/test/java/E2E_Testcases/TC292_P0_E2E_CreateColors_Solid.java
@@ -5,7 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
+import java.io.File;
 import com.WMS_ApplicationPages.CreateNewColorPage;
 import com.WMS_ApplicationPages.DashboardPage;
 import com.WMS_ApplicationPages.MainMenuEnum;
